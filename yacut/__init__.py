7a9d0876de4ba84faed74f8666ab875edf32f5b8
Kalanimus <nikita.kalan@yandex.ru>
2026-07-28 01:11:18 +0300

    1st try

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,6 +1,4 @@
-from yacut import api_views, views
-from yacut import views
-from flask import Flask
+from yacut import api_views, error_handlers, viewsfrom flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,5 +10,3 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-from yacut import error_handlers, api_views, views  # noqa: E402, F401
